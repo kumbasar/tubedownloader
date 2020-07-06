cb743d1072320ad92fb1af71488056cdcce74b03
Volkan Kumbasar <7557299+kumbasar@users.noreply.github.com>
2020-07-06 20:51:27 +0300

    fix skip arg naming

--- viddownloader.py
+++ viddownloader.py
@@ -9,19 +9,18 @@ parser = argparse.ArgumentParser(
                     formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-u", "--youtube", help = "Youtube URL. Example: 'https://www.youtube.com/watch?v=huTUOek4LgU'", type = str, required= True)
 parser.add_argument("-o", "--output", help = "Output filename", type = str, required= False, default='out.mp4')
-parser.add_argument("-s", "--skip", help = "Skip existing", type = bool, required= False, default=False)
-
+parser.add_argument("-s", "--skip", help = "Skip existing tmp", type = bool, required= False, default=False)
 args = parser.parse_args()
 
 yt = YouTube(args.youtube)
 
 print("Downloading video part:")
 print(yt.streams.filter(file_extension='mp4',type="video").order_by('resolution')[-1])
-yt.streams.filter(file_extension='mp4',type="video").order_by('resolution')[-1].download( output_path='tmp', filename='video', skip_existing=args.force)
+yt.streams.filter(file_extension='mp4',type="video").order_by('resolution')[-1].download( output_path='tmp', filename='video', skip_existing=args.skip)
 
 print("Downloading audio part:")
 print(yt.streams.get_audio_only("mp4"))
-yt.streams.get_audio_only("mp4").download(output_path='tmp', filename='audio', skip_existing=args.force)
+yt.streams.get_audio_only("mp4").download(output_path='tmp', filename='audio', skip_existing=args.skip)
 
 print("Merging")
 video_stream = ffmpeg.input('tmp/video.mp4')
